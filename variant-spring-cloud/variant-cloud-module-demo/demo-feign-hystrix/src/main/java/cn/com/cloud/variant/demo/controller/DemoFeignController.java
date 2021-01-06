6ef0f3d8bcc642dcd36d82b98e8afaa8e7f2de11
mars <strivemars@yeah.net>
2021-01-06 13:59:01 +0800

    Create Cloud Feign Hystrix

---
+++ variant-spring-cloud/variant-cloud-module-demo/demo-feign-hystrix/src/main/java/cn/com/cloud/variant/demo/controller/DemoFeignController.java
@@ -0,0 +1,24 @@
+package cn.com.cloud.variant.demo.controller;
+
+import cn.com.cloud.variant.demo.client.DemoFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ：macx7@chinatelecom.cn
+ * @since ：2021/1/5
+ */
+@RestController
+public class DemoFeignController {
+
+    @Autowired
+    DemoFeignService service;
+
+    @PostMapping("/web/feign")
+    public String ribbonTest(String str) {
+        System.out.println(str);
+        return service.ribbonTest(str);
+    }
+
+}
